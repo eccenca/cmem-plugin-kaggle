043b8b6013780a7ae7c2036368b6e3db180ed40e
Ranga Reddy Nukala <rangareddy@Rangas-Mac.local>
2022-04-08 11:39:08 +0530

    add dependencies

--- tests/test_kaggle.py
+++ tests/test_kaggle.py
@@ -1,14 +1,18 @@
 """Plugin tests."""
-from cmem_plugin_kaggle import DollyPlugin
+from kaggle_import import KaggleImport
+from cmem_plugin_base.dataintegration.discovery import discover_plugins
+
+
+# package prefix
+def test_regestration():
+    plugins = discover_plugins(package_name="cmem_plugin_kaggle")
+    assert len(plugins) > 0
 
 
 def test_execution():
     """Test plugin execution"""
-    entities = 100
-    values = 10
+    url = "http://winterolympicsmedals.com/medals.csv"
 
-    plugin = DollyPlugin(number_of_entities=entities, number_of_values=values)
-    result = plugin.execute()
-    for item in result.entities:
-        assert len(item.values) == len(result.schema.paths)
-
+    plugin = KaggleImport(url=url)
+    plugin.execute()
+    # write assertions
